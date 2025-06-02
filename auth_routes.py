70a92f1dd337558646d56269b12dad0e030af069
Lucas Farias <lucasf1@gmail.com>
2025-06-02 05:49:48 -0300

    Criação de rotas de  login e refresh

--- auth_routes.py
+++ auth_routes.py
@@ -1,12 +1,38 @@
+from datetime import datetime, timedelta, timezone
 from fastapi import APIRouter, Depends, HTTPException
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from models import Usuario
-from schemas import UsuarioSchema
+from schemas import LoginSchema, UsuarioSchema
+
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
+
+
+def criar_token(id_usuario, duracao_token = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dict_info = {"sub": id_usuario, "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dict_info, SECRET_KEY, ALGORITHM)
+
+    return jwt_codificado
+
+def verificar_token(token: str, session: Session = Depends(pegar_sessao)) -> Usuario:
+    # verificar se o token é válido
+    # extrair o id do usuário do token
+    usuario = session.query(Usuario).filter(Usuario.id == 1).first()
+    return usuario
+
+def autenticar_usuario(email: str, senha:str, session: Session):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+    
+    return usuario
 
 
 @auth_router.get('/')
@@ -36,4 +62,40 @@ async def criar_conta(
                                usuarioSchema.ativo, usuarioSchema.admin)
         session.add(novo_usuario)
         session.commit()
-        return {'mensagem': f'usuário cadastrado com sucesso - {usuarioSchema.email}'}
+        return {
+            'mensagem': f'usuário cadastrado com sucesso - {usuarioSchema.email}'
+        }
+
+
+@auth_router.post('/login')
+async def login(
+    login_schema: LoginSchema,
+    session: Session = Depends(pegar_sessao)
+):
+    """
+    Essa rota faz o login do usuário.
+    """
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
+    if not usuario:
+        raise HTTPException(status_code=404, detail='Usuário não encontrado ou credenciais inválidas')
+    else:
+        access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
+        return {
+            'access_token': access_token,
+            'refresh_token': refresh_token,
+            'token_type': 'Bearer',
+        }
+    
+
+@auth_router.get('/refresh')
+async def use_refresh_token(token):
+    # verifica o token
+    usuario = verificar_token(token)
+
+    access_token = criar_token(usuario.id)
+    return {
+        'access_token': access_token,
+        'token_type': 'Bearer',
+    }
+
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from passlib.context import CryptContext
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM', 'HS256')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
 
 app = FastAPI()
 
