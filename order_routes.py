6759db5b15920bc5de244dc6fb665effe6c92cff
Lucas Farias <lucasf1@gmail.com>
2025-06-14 10:13:14 -0300

    uso de admin e lazyload

--- order_routes.py
+++ order_routes.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from dependencies import pegar_sessao
-from models import Pedido
+from dependencies import pegar_sessao, verificar_token
+from models import Pedido, Usuario
 from schemas import PedidoSchema
 
-order_router = APIRouter(prefix='/pedidos', tags=['pedidos'])
+order_router = APIRouter(prefix='/pedidos', tags=['pedidos'], dependencies=[Depends(verificar_token)])
 
 
 @order_router.get('/')
@@ -33,4 +33,35 @@ async def criar_pedido(
     return {
         'mensagem':
             f'Pedido criado com sucesso. ID do pedido: {novo_pedido.id}'
+    }
+
+
+@order_router.post('/pedido/cancelar/{id_pedido}')
+async def cancelar_pedido(
+    id_pedido: int, 
+    session: Session = Depends(pegar_sessao),
+    usuario: Usuario = Depends(verificar_token)
+):
+    # usuario.admin = True
+    # usuario.id = pedido.usuario.id
+
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+    if not pedido:
+        raise HTTPException(
+            status_code=404,
+            detail='Pedido não encontrado'
+        )
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(
+            status_code=401,
+            detail='Você não tem permissão para cancelar este pedido'
+        )
+
+    pedido.status = 'CANCELADO'
+    session.commit()
+
+
+    return{
+        'mensagem': f'Pedido número: {pedido.id} cancelado com sucesso',
+        'pedido': pedido
     }
